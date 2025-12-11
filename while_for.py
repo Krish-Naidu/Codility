17974054e2255e79e87e7ad05b38d56af38d089f
Krish-Naidu <krish.28.naidu@gmail.com>
2025-12-11 20:45:02 +0000

    Refactor calendar and while_for scripts; remove calendar.py and update while_for.py to include reverse iteration

--- while_for.py
+++ while_for.py
@@ -1,11 +1,21 @@
 mylist = [1064, 34, 25, 12, 22, 11, 90, 955]
 length = len(mylist)
-# Iterating using a for loop. 
-for i in range(0,length):
-    print(mylist[i])
+# # Iterating using a for loop. 
+# for i in range(0,length):
+#     print(mylist[i])
 
-# Iterating using a while loop.
-x = 0  
-while x < length:
-    print(mylist[x])
-    x = x + 1 
+# # printing in reverse order using for loop
+# for i in range(length-1, -1, -1):
+#     print(mylist[i])
+
+# # Iterating using a while loop.
+# x = 0  
+# while x < length:
+#     print(mylist[x])
+#     x = x + 1 
+
+# Iterating using a while loop in reverse 
+j = length - 1 
+while j > -1:
+    print(mylist[j])
+    j = j - 1 
